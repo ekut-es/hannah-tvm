765dc1fe70cb2d47ecdf47d99f1f2b5aa7f82542
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2021-04-02 14:43:05 +0200

    Operator test

--- hannah_tvm/compile.py
+++ hannah_tvm/compile.py
@@ -12,24 +12,25 @@ from . import config
 from . import measure
 from . import load
 
-
+logger = logging.getLogger("hannah-tvm-compile")
 
 def compile(config):
     relay_mod, params, inputs = load.load_model(config.model)
-    measure_context = measure.AutomateRPCMeasureContext(config.board)
+    #measure_context = measure.AutomateRPCMeasureContext(config.board)
     
     target = tvm.target.Target(config.board.target)
     target_host = tvm.target.Target(config.board.target_host)
 
     if target.kind == "cuda":
-        autotvm.measure.measure_methods.set_cuda_target_arch(target.attrs["arch"])
+        if "arch" in target.attrs:
+            autotvm.measure.measure_methods.set_cuda_target_arch(target.attrs["arch"])
+        else:
+            logger.warning("CUDA target has no architecture attribute")
 
+    print(relay_mod)    
 
-    # target = "llvm"
-    # target_host = "llvm"
-    # ctx = tvm.cpu(0)
-    # with tvm.transform.PassContext(opt_level=3):
-    #     lib = relay.build(relay_mod, target=target, target_host=target_host, params=params)
+    with tvm.transform.PassContext(opt_level=3):
+         lib = relay.build(relay_mod, target=target, target_host=target_host, params=params)
 
     #for batch in dataset:
     #    output = model(batch)
---
+++ test/test_operators.py
@@ -0,0 +1,40 @@
+import logging
+import tvm
+import pytest
+import tvm
+from tvm import topi, te, micro
+import numpy as np
+
+def run_conv2d_bitparallel(B, C, H, W, conv_size, bits, target):
+    max_value = 2^(bits-1)-1
+    min_value = -2^(bits-1)
+
+    input = np.random.randint(min_value, max_value, (B,C,H,W))
+    weight = np.random.randint(min_value, max_value, (C, C, conv_size, conv_size))
+
+    ph_input = te.placeholder(input.shape, name="input", dtype=f'int{bits}')
+    ph_weight = te.placeholder(weight.shape, name="weight", dtype=f'int{bits}')
+                          
+    conv = topi.nn.conv2d(ph_input,
+                          ph_weight, 
+                          strides=(1,1),
+                          padding=(conv_size//2, conv_size//2),
+                          dilation=(0,0))
+
+    s_conv = te.create_schedule(conv.op)
+    print(tvm.lower(s_conv, [ph_input, ph_weight], simple_mode=True))
+
+    with tvm.transform.PassContext(opt_level=3):
+        tvm.build()
+
+    target = tvm.target.Target(f"{target}")
+    compiler = tvm.micro.DefaultCompiler(target=target)
+    opts = tvm.micro.default_options(
+        os.path.join(tvm.micro.get_standalone_crt_dir(), "template", "host")
+    )
+
+    
+
+
+if __name__ == "__main__":
+    run_conv2d_bitparallel(1, 8, 24, 24, 3, bits=4, target="llvm")
