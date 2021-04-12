860ffbf9f9718da1da3ddbc20118c523f4a7db43
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2021-04-13 01:18:59 +0200

    Fix config

--- hannah_tvm/config.py
+++ hannah_tvm/config.py
@@ -6,15 +6,27 @@ from hydra.core.config_store import ConfigStore
 from omegaconf import MISSING
 
 @dataclass
+class HardwareParams:
+    num_cores: int = 4
+    vector_unit_bytes: int  = 64
+    cache_line_bytes: int  = 64
+    max_shared_memory_per_block: int  = 65536 
+    max_local_memory_per_block: int  = 2147483647
+    max_threads_per_block: int  = 1024
+    max_vthread_extent: int = 8 # 32 / 4
+    warp_size: int  = 32
+
+@dataclass
 class Board:
     name: Any = MISSING
-    target: Any = "cuda -arch=sm_62" # "llvm -mtriple=aarch64-linux-gnu -device=arm_cpu -model=jetsontx2"
-    target_host: Any = "llvm -mtriple=aarch64-linux-gnu -device=arm_cpu -model=jetsontx2" 
+    target: Any = "llvm"
+    target_host: Any = "llvm" 
     tracker: Optional[str] = None
     opencl: bool = True
     cuda: bool = True
     rebuild_runtime: bool = False
-    
+    hardware_params: HardwareParams = HardwareParams()
+    micro: bool = False    
 
 @dataclass
 class Model:
@@ -22,9 +34,24 @@ class Model:
     input_shapes: Any = None  
 
 @dataclass
+class QConfig:
+    engine: str = MISSING 
+    calibrate_mode: str = "global_scale"
+    global_scale: float = 8.0
+    weight_scale: str = 'max'
+    skip_dense_layer: bool = False
+    skip_conv_layers: Any = field(default_factory=list)
+    do_simulation: bool = False
+    round_for_shift: bool =  False
+    rounding: str = "UPWARD"
+    partition_conversions: str = "enabled"
+
+    
+@dataclass
 class Config:
     board: Board = Board()
     model: Model = Model()
+    qconfig: Optional[QConfig] = None
 
 cs = ConfigStore.instance()
 cs.store(name="config", node=Config)
--- hannah_tvm/tune.py
+++ hannah_tvm/tune.py
@@ -21,19 +21,25 @@ def compile(config):
     target = tvm.target.Target(config.board.target)
     target_host = tvm.target.Target(config.board.target_host)
 
-    autotvm.measure.measure_methods.set_cuda_target_arch(target.attrs["arch"])
+    if target.kind == "cuda":
+        if "arch" in target.attrs:
+            autotvm.measure.measure_methods.set_cuda_target_arch(target.attrs["arch"])
+        else:
+            logger.warning("CUDA target has no architecture attribute")
 
     logging.info("Extracting tasks ...")
-    tasks, task_weights = auto_scheduler.extract_tasks(relay_mod["main"], params, target, target_host)
+    hardware_params=None
+    if config.board.get('hardware_params', None):
+        hardware_params = auto_scheduler.HardwareParams(**hardware_params)
+
+    tasks, task_weights = auto_scheduler.extract_tasks(relay_mod["main"], params, target, target_host,  hardware_params=hardware_params)
 
     for idx, task in enumerate(tasks):
         logging.info("========== Task %d  (workload key: %s) ==========" % (idx, task.workload_key))
         logging.info(task.compute_dag)
 
-
     runner = measure_context.runner
     
-
     logging.info("Begin tuning...")
     log_file = f"{config.board.name}.log"
     tuner = auto_scheduler.TaskScheduler(tasks, task_weights)
@@ -46,7 +52,7 @@ def compile(config):
     tuner.tune(tune_option)
 
 
-@hydra.main(config_name="config")
+@hydra.main(config_name="config", config_path="conf")
 def main(config):
     return compile(config)
 
