c5e00ae84169144f560190139c58607ef78a8bb7
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2021-04-27 10:05:37 +0200

    Use cuda

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ echo "Installing tvm"
 mkdir -p external/tvm/build
 cp tvm_config.cmake external/tvm/build/config.cmake
 pushd external/tvm/build
-cmake --cmake-force-configure .. -G Ninja -DCMAKE_INSTALL_PREFIX=$VIRTUAL_ENV/ -DCMAKE_CXX_COMPILER=$(which g++-8)
+cmake --cmake-force-configure .. -G Ninja -DCMAKE_INSTALL_PREFIX=$VIRTUAL_ENV/ -DCMAKE_CXX_COMPILER=$CXX_COMPILER -DUSE_CUDA=$USE_CUDA
 cmake --build .
 popd
 pip install -e external/tvm/python 
