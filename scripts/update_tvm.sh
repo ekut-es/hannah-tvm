ff13e529b25b7597ddc6eb965356a742a9853357
Christoph Gerum <christoph.gerum@uni-tuebingen.de>
2021-06-15 15:32:42 +0200

    Update patch

--- scripts/update_tvm.sh
+++ scripts/update_tvm.sh
@@ -2,12 +2,20 @@
 
 
 pushd external/tvm
+echo "Updating patch"
 git format-patch main --stdout > ../../patches/tvm.patch
+echo "Removing patch"
+git apply -R ../../patches/tvm.patch
+echo "Update tvm"
 git checkout main
 git pull 
 git submodule update --init --recursive
 popd
+
+echo "Commiting new tvm"
 git commit -m "Update tvm" external/tvm
+
+echo "Applying patch"
 pushd external/tvm
 git apply ../../patches/tvm.patch
 popd
